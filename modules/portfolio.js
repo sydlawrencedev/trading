460d407df102faf6c7f067f0044f07f1a212d12f
Syd Lawrence <sydlawrence@gmail.com>
2021-01-03 14:02:17 +0000

    added hodl to status message

--- modules/portfolio.js
+++ modules/portfolio.js
@@ -129,7 +129,7 @@ portfolio.logStatus = async function(time = settings.timeWindow.start) {
     ]);
     var params = [];
     var obj = {
-        portfolio: "$"+Math.round(this.portfolioValue),
+        portfolio: "$"+Math.round(this.portfolioValue) + " vs " + hodlAverage,
         strategy: this.strategies[0].description,
         stocklist: settings.stockFile
     };
