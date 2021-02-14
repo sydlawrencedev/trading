5c2b0eda8c683b0df7b258ecf3bf98d8ea1a661c
Syd Lawrence <sydlawrence@gmail.com>
2021-02-14 14:30:59 +0000

    added boomer stocks

--- settings.js
+++ settings.js
@@ -5,10 +5,9 @@ var chalk = require("chalk");
 var verbose = false;
 if (process.argv[2] == "-verbose" || process.argv[1].indexOf("index") > -1) {
     verbose = true;
-    console.log(chalk.green("Running in verbose mode"));
 }
 
-stocks = "watch2020";
+stocks = "spy";
 strategy = "test";
 
 // stocks = "watch";
@@ -33,8 +32,9 @@ settings = {
     alphavantagekey: keys.alphavantage.key,
     alpaca: keys.alpaca,
     supportPartialShares: false,
+    analyze: true,
     startingCapital: 100000,
-    verbose: verbose,
+    verbose: verbose, //verbose,
     tradingTimeout: 1 * 60 * 1000,
     timeframe: "daily", // daily, minute
     interval: false, // 1min, 5min, 15min, 30min, 60min, false if daily
@@ -44,7 +44,7 @@ settings = {
         start: "2020-01-01 00:00:00",
         end: "2020-12-01 00:00:00",
 
-        start: "2020-01-01 00:00:00",
+        start: "2020-11-01 00:00:00",
         end: "2020-12-31 00:00:00",
         // start: false // use all available data
     },
@@ -53,6 +53,10 @@ settings = {
         sell: 0
     },
     cashBaseWeighting: 50
+}
+
+if (settings.verbose) {
+    console.log(chalk.green("Running in verbose mode"));
 }
 
 if (settings.timeframe == "daily") {
